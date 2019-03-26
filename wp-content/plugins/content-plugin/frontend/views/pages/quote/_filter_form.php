c16717a4063e02080e4c6f8ce3b1fe09a90b27c3
Manoj Roka <roka.manoj12@gmail.com>
2019-03-26 16:49:43 +0545

    quote label has been changed

--- wp-content/plugins/content-plugin/frontend/views/pages/quote/_filter_form.php
+++ wp-content/plugins/content-plugin/frontend/views/pages/quote/_filter_form.php
@@ -21,16 +21,50 @@
 
 <?php
 if (isset($submit_form)) { lcmf_popup_form_open($module_detail); ?>
-    <label>Title*</label>
-    <input class="form-control limit_word" type="text" name="title" id="title" required>
-    <label>Quote*</label>
-    <?php lcm_editor("", "quote_description", array('textarea_rows' => '8')); ?>
-    <?php _lcm_dropdown_selector('status', NULL, array('Suggested', 'New', 'Published', 'Hidden', 'Spam'), 'Status*') ?>
-    <label>Headshot*</label>
-    <input class="form-control" type="file" name="headshot" accept="image/*" required>
-    <?php _lcmf_name_email_company_input() ?>
-    <label>Company Website</label>
-    <input class="form-control" type="url" name="company_website">
+    
+    <div>
+        <label>Quote Title</label>
+        <input class="form-control limit_word" type="text" name="title" id="title" required>
+    </div>
+    
+    <div>
+        <label>Author</label>
+        <input class="form-control" type="text" name="name" required>
+    </div>
+    
+    <div>
+        <label>Author Picture</label>
+        <input class="form-control" type="file" name="headshot" accept="image/*" required>
+    </div>
+    
+    <div>
+        <label>Author Company's</label>
+        <input class="form-control" type="text" name="company" required>
+    </div>
+    
+    <div>
+        <label>Company Url</label>
+        <input class="form-control" type="url" name="company_website">
+    </div>
+    
+    <div>
+        <label>Author Job Positoin</label>
+        <input class="form-control limit_word" type="text" required>
+    </div>
+        
+    <!--<div>-->
+        <!--<label>Email*</label>-->
+        <input class="form-control" type="hidden" name="email" value="test@test.com" required>
+    <!--</div>-->
+        
+    <div>
+        <label>Content</label>
+        <?php lcm_editor("", "quote_description", array('textarea_rows' => '8')); ?>
+    </div>
+    
+    <?php // _lcm_dropdown_selector('status', NULL, array('Suggested', 'New', 'Published', 'Hidden', 'Spam'), 'Status*') ?>
+    <?php // _lcmf_name_email_company_input() ?>
+    
     <?php lcmf_popup_form_closed($module_detail);
 } ?>
 
