f7033e69e1c20a138f26633eba451288df708336
Manoj Roka <roka.manoj12@gmail.com>
2019-03-26 15:40:56 +0545

    design changes

--- wp-content/plugins/content-plugin/frontend/views/pages/quote/_filter_form.php
+++ wp-content/plugins/content-plugin/frontend/views/pages/quote/_filter_form.php
@@ -1,30 +1,32 @@
-<div class="lcm_row lcm_border_around">
-    <?php if(isset($filter)){ ?>
+<!--<div class="lcm_row lcm_border_around">-->
+    <?php if (isset($filter)) { ?>
         <div class="lcm_col-md-7">
-                </div>
-                <div class="lcm_col-md-2">
+        </div>
+        <div class="lcm_col-md-2">
 
-                </div>
-                <div class="lcm_col-md-1">
-                    <P>OR</p>
-                </div>
+        </div>
+        <div class="lcm_col-md-1">
+            <P>OR</p>
+        </div>
     <?php } ?>
     <div class="lcm_col-md-2">
-    <?php if(isset($submit_form)){ 
-        lcmf_popup_form_open($module_detail); 
-    ?>
-    </div>
-</div>
-
-        <label>Title*</label>
-        <input class="form-control limit_word" type="text" name="title" id="title" required>
-        <label>Quote*</label>
-        <?php lcm_editor("", "quote_description", array('textarea_rows' => '8')); ?>
-        <?php _lcm_dropdown_selector('status', NULL, array('Suggested', 'New', 'Published', 'Hidden', 'Spam'), 'Status*' ) ?>
-        <label>Headshot*</label>
-        <input class="form-control" type="file" name="headshot" accept="image/*" required>
-        <?php _lcmf_name_email_company_input()?>
-        <label>Company Website</label>
-        <input class="form-control" type="url" name="company_website">
-    <?php lcmf_popup_form_closed($module_detail); } ?>
+        <?php
+        if (isset($submit_form)) {
+            lcmf_popup_form_open($module_detail);
+            ?>
+        </div>
+<!--    </div>-->
+    <label>Title*</label>
+    <input class="form-control limit_word" type="text" name="title" id="title" required>
+    <label>Quote*</label>
+    <?php lcm_editor("", "quote_description", array('textarea_rows' => '8')); ?>
+    <?php _lcm_dropdown_selector('status', NULL, array('Suggested', 'New', 'Published', 'Hidden', 'Spam'), 'Status*') ?>
+    <label>Headshot*</label>
+    <input class="form-control" type="file" name="headshot" accept="image/*" required>
+    <?php _lcmf_name_email_company_input() ?>
+    <label>Company Website</label>
+    <input class="form-control" type="url" name="company_website">
+    <?php lcmf_popup_form_closed($module_detail);
+}
+?>
 
